927712b13a6c104c57600f0a9f017c56bcac59f1
LeanderMattle <leander.mattle@outlook.com>
2024-07-24 01:31:29 +0200

    Levelgenerator finished
    the levelgenerator is done, the game works though loading times go up by size 17
    
    maybe splitting up the process is useful

--- scripts/generator.gd
+++ scripts/generator.gd
@@ -2,21 +2,48 @@ extends Node2D
 
 @export var size: int = 7
 var n: int = 0
+var l: int
+var width: int
+var hight: int
 var ends: Array[int]
 var pos: Array
 var hex = preload("res://scenes/hex.tscn")
+<<<<<<< Updated upstream
 enum Ingredient{Herb1, Herb2, Herb3, Shroom1, Shroom2, Shroom3, Salt1, Salt2, Salt3, Flamel}
 @export var ingredient = [3,0,0,3,0,0,3,0,0,1]
+=======
+signal talkToNeighbor(ownpos: int, owningr: int, neighborpos:int)
+@export var ingredientStack: Dictionary = {
+	"Herb1" = 3,
+	"Herb2" = 0,
+	"Herb3" = 0,
+	"Shroom1" = 3,
+	"Shroom2" = 0,
+	"Shroom3" = 0,
+	"Salt1" = 3,
+	"Salt2" = 0,
+	"Salt3" = 0,
+	"Flamel" = 1,
+}
+>>>>>>> Stashed changes
 
-signal Populated
 
 
 func _ready():
 	n = 1 - (3 * size) + (3 * (size * size))
+	l = 2 * size - 1
+	width = l * 48 + 100
+	hight = l * 35 + 150
+	$PanelContainer.size = Vector2i(width, hight)
+	get_viewport()
 	get_ends()
 	def_hex()
 	populate()
+<<<<<<< Updated upstream
 	Populated.emit()
+=======
+	signalBus.populated.emit()
+>>>>>>> Stashed changes
 
 
 	# calculates where the line breaks in the hex grid
@@ -44,17 +71,24 @@ func def_hex():
 func populate():
 	var y: int = 0
 	while 1:
-		if y == ingredient.size():
+		if y == ingredientStack.size():
 			break
 		else:
 			y = 0
+<<<<<<< Updated upstream
 		for i in ingredient.size():
 			if ingredient[i] != 0:
 				var x = randi_range(0, n)
+=======
+		for i in ingredientStack.keys():
+			if ingredientStack[i] != 0:
+				var x = randi_range(0, n-1)
+>>>>>>> Stashed changes
 				if pos[x].is_empty():
 					pos[x].ingredient = i
-					ingredient[i] -= 1
+					ingredientStack[i] -= 1
 			else:
 				y += 1
 	for i in n:
+		pass
 		pos[i].neighborhood()
