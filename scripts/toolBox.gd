fa43af83f03bdf6dab58139004ea1a967be090d1
LeanderMattle <leander.mattle@outlook.com>
2024-08-29 16:08:14 +0200

    toolBox basics

--- scripts/toolBox.gd
+++ scripts/toolBox.gd
@@ -3,16 +3,26 @@ extends Control
 var held: bool = false
 var holdable: bool = true
 var speed: float = 25
+@export var upy: float = 400
+@export var downy: float = 20
+var openy: float = 450
+var closey: float = 670
 
 func _ready() -> void:
 	signalBus.deactivate.connect(deactivate)
+	set_physics_process(false)
+	held = false
+	global_position.y = 720 - downy
 
 func deactivate(r) -> void:
 	held = false
 	holdable = r
 
 func _physics_process(delta) -> void:
-	global_position.y = lerp(global_position.y, get_global_mouse_position().y , speed * delta)
+	var miny: float = 720 - upy
+	var maxy: float = 720 - downy
+	global_position.y = clamp(lerp(global_position.y, get_global_mouse_position().y , speed * delta), miny, maxy)
+	held = is_equal_approx(global_position.y, get_global_mouse_position().y) or global_position.y > closey or global_position.y < openy
 	set_physics_process(held)
 
 func _on_pick_up_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
