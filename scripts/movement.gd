dfc4eecdb7abdcd499092f5f00258a20bc31f10b
ArtemisMattle <leander.mattle@outlook.com>
2024-12-06 16:32:45 +0100

    movement and tooltip shenanigans

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -4,6 +4,8 @@ const maxZoom: float = 2.0
 const zoomInc: float = 0.05
 const zoomRate: float = 7.0
 var tZoom: float = 2.0
+var direction: Vector2 = Vector2.ZERO
+var speed: float = 21
 
 func _ready() -> void:
 	globalVariables.cam = self
@@ -14,22 +16,27 @@ func _physics_process(delta):
 		set_physics_process(not is_equal_approx(zoom.x, tZoom))
 
 func _unhandled_input(event: InputEvent):
+	direction = Vector2.ZERO
 	if not globalVariables.paused:
-		if Input.is_action_pressed("pan"):
-			if event is InputEventMouseMotion:
-				position -= event.relative / zoom
+		#if Input.is_action_pressed("pan"):
+			#if event is InputEventMouseMotion:
+				#position -= event.relative / zoom
 		if Input.is_action_pressed("zoom in"):
 			zoom_in()
 		if Input.is_action_pressed("zoom out"):
 			zoom_out()
 		if Input.is_action_pressed("move up"):
-			pass
+			direction = direction + Vector2.UP
 		if Input.is_action_pressed("move down"):
-			pass
+			direction = direction + Vector2.DOWN
 		if Input.is_action_pressed("move right"):
-			pass
+			direction = direction + Vector2.RIGHT
 		if Input.is_action_pressed("move left"):
-			pass
+			direction = direction + Vector2.LEFT
+		
+		if direction != Vector2.ZERO:
+			direction = direction.normalized()
+			position += direction * speed 
 
 func zoom_in():
 	tZoom = min(tZoom + zoomInc, maxZoom)
