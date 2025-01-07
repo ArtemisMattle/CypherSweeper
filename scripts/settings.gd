6fafd857350bc567350651fb9949a89ba0f58b2e
ArtemisMattle <leander.mattle@outlook.com>
2025-01-07 04:26:11 +0100

    smol fixes and fullscreen

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -4,5 +4,18 @@ var masterMute: bool = false
 var musicMute: bool = false
 var soundMute: bool = false
 
+var fs: bool = false
+
 enum sMode {normal, fast, zippy}
 var speedMode: int = sMode.normal
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		fs = not fs
+		tFullscreen()
+
+func tFullscreen() -> void:
+	if fs:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
